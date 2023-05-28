3413ff8fe723f2063510309fdc4350ccf754eb54
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2023-05-28 08:06:34 +0200

    api routing done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,32 @@
-import { Inter } from 'next/font/google'
-import CallToAction from '@/components/UI/CallToAction'
-import Services from '@/pages/services'
-import About from '@/pages/About'
-import Map from '@/components/UI/Map'
-import MoreOnTrusts from '@/components/UI/MoreOnTrusts'
-import ContactUs from '@/pages/ContactUs'
+import { Inter } from "next/font/google";
+import CallToAction from "@/components/UI/CallToAction";
+import Services from "@/pages/services";
+import About from "@/pages/About";
+import Map from "@/components/UI/Map";
+import MoreOnTrusts from "@/components/UI/MoreOnTrusts";
+import ContactUs from "@/pages/ContactUs";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => {
-  return (
-    <main
-      className={` ${inter.className}`}
-    >
-      <CallToAction/>
-      <Services/>
-      <About/>
-      <Map/>
-      <MoreOnTrusts/>
-      <ContactUs/>
-    </main>
-  )
-}
+	return (
+		<>
+			<Head>
+				<title>MTA</title>
+				<meta name="description" content="MTA" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>{" "}
+			<main className={` ${inter.className}`}>
+				<CallToAction />
+				<Services />
+				<About />
+				<Map />
+				<MoreOnTrusts />
+				<ContactUs />
+			</main>
+		</>
+	);
+};
 
-export default Home
+export default Home;
