390b681d9388e2011684f25d4f620bf79e339637
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2023-05-30 19:31:32 +0200

    ammended the privacy policy

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -3,78 +3,140 @@ import Heading from "@/components/UI/Heading";
 const PrivacyPolicy = () => {
 	return (
 		<article className=" center mx-16 mt-10">
-			<Heading
-				content="Privacy Policy"
-			/>		
+			<Heading content="Privacy Policy" />
+			<p className="leading-6 my-5">Effective Date: 30/05/2023</p>
+
+			<h2 className="leading-6 my-5">1. Information We Collect</h2>
 			<p className="leading-6 my-5">
-				This privacy policy applies to ezdev.solutions and explains how I use
-				any personal information I collect about you when you use this website.
+				We may collect and process the following types of personal information:
 			</p>
-			<span className="leading-6 my-5">Topics:</span>
-			<ul className="list-decimal list-inside">
-				<li className="indent-4">What information do I collect about you?</li>
-				<li className="indent-4">How will I use the information about you?</li>
-				<li className="indent-4">Access to your information and corrections</li>
-				<li className="indent-4">Cookies</li>
-				<li className="indent-4">Other websites</li>
-				<li className="indent-4">Changes to my privacy policy</li>
-				<li className="indent-4">How to contact me</li>
+			<ul className="leading-6 my-5">
+				<li className="leading-6 my-5">
+					Personal Information Provided by You: We may collect personal
+					information such as your name, email address, contact number, physical
+					address, and any other information you voluntarily provide when you
+					contact us or use our services.
+				</li>
+				<li className="leading-6 my-5">
+					Usage Information: We may collect information about your usage of our
+					website, including your IP address, browser type, referring/exit
+					pages, and operating system. We use this information to analyze
+					trends, administer our website, track user movements, and gather broad
+					demographic information for aggregate use.
+				</li>
 			</ul>
-			<p className="leading-6 my-5">What information do I collect about you?</p>
+
+			<h2 className="leading-6 my-5">2. Use of Personal Information</h2>
 			<p className="leading-6 my-5">
-				I collect information about you when you complete a form on my website.
-				The information you give me may include your name, company name and
-				email address.
+				We may use the personal information we collect for the following
+				purposes:
 			</p>
-			<p className="leading-6 my-5">How will I use the information about you?</p>
-			<p className="leading-6 my-5">
-				I collect information about you to gain better understanding of how
-				users are using my website to better my personal development and to give
-				the best user experience that I possibly can. If I am contacted by you
-				through my website, I will use the information provided to do so
-				accordingly.
-			</p>
-			<p className="leading-6 my-5">I use your information to:</p>
-			<ul>
-				<li>Contact you if you have requested me to.</li>
-				<li>Monitor site activity.</li>
+			<ul className="leading-6 my-5">
+				<li className="leading-6 my-5">
+					Provision of Services: To provide our trust administration services,
+					communicate with you, and respond to your inquiries.
+				</li>
+				<li className="leading-6 my-5">
+					Website Improvement: To analyze and improve our website, enhance user
+					experience, and troubleshoot any technical issues.
+				</li>
+				<li className="leading-6 my-5">
+					Marketing Communication: With your consent, we may send you
+					promotional emails or other marketing materials about our services.
+					You can opt-out of receiving such communications at any time.
+				</li>
+				<li className="leading-6 my-5">
+					Legal Compliance: To comply with applicable laws, regulations, and
+					legal processes.
+				</li>
 			</ul>
+
+			<h2 className="leading-6 my-5">3. Sharing of Personal Information</h2>
 			<p className="leading-6 my-5">
-				If you change your mind about being contacted in the future, please let
-				us know.
+				We may share your personal information with third parties in the
+				following circumstances:
 			</p>
-			<p className="leading-6 my-5">Access to your information and corrections</p>
+			<ul className="leading-6 my-5">
+				<li className="leading-6 my-5">
+					Service Providers: We may engage trusted third-party service providers
+					who assist us in operating our business and providing our services.
+					These service providers are obligated to maintain the confidentiality
+					and security of your personal information.
+				</li>
+				<li className="leading-6 my-5">
+					Legal Requirements: We may disclose your personal information if
+					required to do so by law or in response to a valid request from a
+					governmental authority, court order, or similar legal process.
+				</li>
+				<li className="leading-6 my-5">
+					Business Transfers: In the event of a merger, acquisition, or sale of
+					all or a portion of our assets, your personal information may be
+					transferred to the acquiring entity.
+				</li>
+			</ul>
+
+			<h2 className="leading-6 my-5">4. Data Security</h2>
 			<p className="leading-6 my-5">
-				You have the right to request a copy of the information that I hold
-				about you if you have contacted me before. If you would like a copy of
-				some or all of your personal information, please email me at
-				anthony@ezdev.solutions
+				We implement reasonable security measures to protect your personal
+				information from unauthorized access, loss, misuse, or alteration.
+				However, please note that no data transmission or storage system can be
+				guaranteed to be 100% secure.
 			</p>
-			<p className="leading-6 my-5">Cookies</p>
+
+			<h2 className="leading-6 my-5">5. Your Rights</h2>
+			<ul className="leading-6 my-5">
+				<li className="leading-6 my-5">
+					Access and Correction: You have the right to access and correct any
+					personal information we hold about you. If you wish to exercise this
+					right, please contact us using the information provided below.
+				</li>
+				<li className="leading-6 my-5">
+					Opt-out: You can opt-out of receiving marketing communications from us
+					by following the unsubscribe instructions included in the emails or by
+					contacting us directly.
+				</li>
+			</ul>
+
+			<h2 className="leading-6 my-5">6. Children's Privacy</h2>
 			<p className="leading-6 my-5">
-				I reserve the right to use cookies to give you the best experience on my
-				website and to help improve the overall user experience. A cookie is a
-				small file that is placed on your computer or mobile device when you
-				visit a website. You can manage or delete cookies as you wish.
+				Our website and services are not directed toward individuals under the
+				age of 18. We do not knowingly collect personal information from
+				children. If you believe we have inadvertently collected personal
+				information from a child, please contact us immediately, and we will
+				take appropriate measures to delete such information.
 			</p>
-			<p className="leading-6 my-5">Other websites</p>
+
+			<h2 className="leading-6 my-5">7. Changes to the Privacy Policy</h2>
 			<p className="leading-6 my-5">
-				My website may contain links to other websites. This privacy policy only
-				applies to this website, so when you link to other websites you should
-				read their own privacy policies.
+				We may update this Privacy Policy from time to time to reflect changes
+				in our practices or legal requirements. We will post any revised Privacy
+				Policy on our website and indicate the effective date. Your continued
+				use of our website and services after the effective date constitutes
+				your acceptance of the revised Privacy Policy.
 			</p>
-			<p className="leading-6 my-5">Changes to my privacy policy</p>
+
+			<h2 className="leading-6 my-5">8. Contact Us</h2>
 			<p className="leading-6 my-5">
-				I keep my privacy policy under regular review and I will place any
-				updates on this web page. This privacy policy was last updated on
-				16/05/2023.
+				If you have any questions or concerns about this Privacy Policy or our
+				data practices, please contact us at:
 			</p>
-			<p className="leading-6 my-5">How to contact me</p>
 			<p className="leading-6 my-5">
-				Please contact me if you have any questions about my privacy policy or
-				the information I hold about you.
+				Mumbi Trust Administrators
+				<br />
+				70 Markotter Street
+				<br />
+				Centurion
+				<br/>
+				Pretoria
+				<br/>
+				Gauteng
+				<br/>
+				0157
+				<br/>
+				<a href="mailto: deon@mumbi.co.za">deon@mumbi.co.za</a>
+				<br/>
+				<a href="tel:+27814868538">+27814868538</a>
 			</p>
-			<p className="leading-6 my-5">Email: anthony@ezdev.solutions</p>
 		</article>
 	);
 };
