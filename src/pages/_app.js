a6f2300543a13a90a26189c7e2f49185ada0f449
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2023-06-01 08:00:34 +0200

    amended analytics tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -52,6 +52,7 @@ const App = ({ Component, pageProps }) => {
 					name="viewport"
 					content="width=device-width, initial-scale=1, maximum-scale=1"
 				/>
+				<link rel="shortcut icon" href="/media/images/MTA_logo.webp" />
 			</Head>
 			<Navigation />
 			<Component {...pageProps} />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,6 @@ const Document = () => {
 		<Html lang="en">
 			<Head>
 				<link rel="shortcut icon" href="/media/images/MTA_logo.webp" />
-			
 			</Head>
 			<body>
 				<Main />
